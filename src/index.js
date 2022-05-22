8774f71f33caeb586a0a8302f055971fc8a3093b
jacarcelenb <carcelenjorge17@gmail.com>
2022-05-22 16:50:30 -0500

    make it better

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.use(require('./routes/index'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
+console.log("Server on port 5000")
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const {Router} = require('express')
+const { getContactos, getContactoByid, postCreateContactos, deleteContacto } = require('../controllers/index.controllers')
+const router = Router()
+
+router.get("/contactos",getContactos)
+router.get("/contactos/:id",getContactoByid)
+router.post("/contactos",postCreateContactos)
+router.delete("/contactos/:id",deleteContacto)
+module.exports=router
