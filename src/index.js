0209031d8626527af2dbed475f2e8f11927097ac
Jessica Cachipuendo <jmcachipuendoc@utn.edu.ec>
2022-05-27 15:59:20 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 app.use(require('./routes/index'))
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
 console.log("Server on port 5000")
